4979dd21417ddd6e57105244970c9183024b96d1
Marlo van Gulik <mjvangulik208@gmail.com>
2020-01-07 13:04:37 +0100

    Backup - classes are epic

--- data/JS/main.js
+++ data/JS/main.js
@@ -1,60 +1,55 @@
-/*
+class Game {
+    // Constructor:
+    constructor() {
+        console.log("Starting game");
+        // Generate html objects
+        this._usedHTMLObjects.guesses = document.getElementById("guesses");
+        for(var wordNumber = 0; wordNumber < 5; wordNumber++) {
+            this._usedHTMLObjects.individualGuessedLetterObjects.push([]);
+            var lineDiv = document.createElement("div");
+            lineDiv.className = "blockLine";
 
-foundLetters: {isInPosition: (true/false), position: (index position), letter: (Letter)};
-
-*/
-
-var inputBox;
-var inputButton;
-var outputBox;
-
-var game = {
-    chosenWordIndex: 0,
-    foundLetters: [],
-    update: function(input) {
-        for(var inputCharNumber = 0; inputCharNumber < input.length; inputCharNumber++) {
-            for(var chosenWordCharNumber = 0; chosenWordCharNumber < words[this.chosenWordIndex].length; chosenWordCharNumber++) {
-                if(input.charAt(inputCharNumber) == words[this.chosenWordIndex].charAt(chosenWordCharNumber)) {
-                    this.foundLetters.push({isInPostion: false, position: inputCharNumber, letter: words[this.chosenWordIndex].charAt(chosenWordCharNumber)});
-                }
+            for(var letterNumber = 0; letterNumber < 5; letterNumber++) {
+                var obj = document.createElement("p");
+                obj.className = "block";
+                obj.innerHTML = " ";
+                
+                this._usedHTMLObjects.individualGuessedLetterObjects[wordNumber].push(obj);
+                lineDiv.appendChild(obj);
             }
+            this._usedHTMLObjects.guesses.appendChild(lineDiv);
         }
-        for(var i = 0; i < this.foundLetters.length; i++) {
-            if(this.foundLetters[i].letter == words[this.chosenWordIndex].charAt(this.foundLetters[i].position)) {
-                this.foundLetters[i].isInPostion = true;
-            }
-            for(var j = 0; j < this.foundLetters.length; j++) {
-                if(this.foundLetters[i].position == this.foundLetters[j].position && this.foundLetters[i].letter == this.foundLetters[j].letter && i != j) {
-                    this.foundLetters.splice(j, 1);
-                }
-            }
-        }
-    },
-    init: function() {
-        this.chosenWordIndex = Math.floor(Math.random() * words.length);
-        this.foundLetters = new Array;
-        console.log(words[this.chosenWordIndex]);
-    },
-}
 
-function init() {
-    console.log("Initiating game");
-    inputBox = document.getElementById("inputText");
-    inputBox
-    inputButton = document.getElementById("inputButton");
-    inputButton.onclick = function() {
-        update();
-        return false;
+        this._usedHTMLObjects.input = document.getElementById("inputText");
+        
+
+        // Clear invisible info
+        
     }
-    outputBox = document.getElementById("words");
-    game.init();
-}
-function update() {
-    if(inputBox.value.length != 5) {
-        inputBox.value = "STRING NOT CORRECT!";
-        console.error("String is not correct!");
-        return false;
+
+    // Start new game (Reset info, counter, etc)
+    start() {
+        this._word = words[Math.floor(Math.random() * words.length)];
+        this._guessCounter = 0;
+        this._guessedLetters = [];
+        this._hasStarted = true;
     }
-    game.update(inputBox.value);
-    inputBox.value = "";
+
+    // Update visible game data, the logic behind the game
+    update() {
+
+    }
+    
+    // private
+    _word = [];
+    _guessCounter = 0;
+    _guessedLetters = [];
+    _hasStarted = false;
+
+    _usedHTMLObjects = {
+        input: "",
+        individualGuessedWordObjects: [],
+        individualGuessedLetterObjects: [],
+        individualCorrectLetterObjects: [],
+    }
 }
